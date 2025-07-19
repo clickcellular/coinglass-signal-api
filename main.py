24abd8b736bb420eb9e3bd570d6e2e414481c119
clickcellular <clickcellularllc@gmail.com>
2025-07-18 21:43:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ app = FastAPI()
 
 COINGLASS_API_KEY = os.environ.get("COINGLASS_API_KEY")
 
-@app.get("/liquidations")
-def get_liquidations(symbol: str = Query("ETH")):
-    url = "https://open-api.coinglass.com/public/v2/liquidation_history"
+@app.get("/api/funding_rate")
+def get_funding_rate(symbol: str = Query("BTC")):
+    url = "https://open-api.coinglass.com/public/v2/funding_rate"
     headers = {
         "coinglassSecret": COINGLASS_API_KEY
     }
     params = {
-        "symbol": symbol,
-        "interval": "15m",
-        "currency": "USDT"
+        "symbol": symbol
     }
 
     response = requests.get(url, headers=headers, params=params)
